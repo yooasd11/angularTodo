4313b854f2173b2f9502ce3a2d12402e82de61d5
Jaeyoon <jaeyoon@bluehole.net>
2018-11-15 12:13:50 +0900

    add dependency for redux

--- src/app/github/search/search.component.ts
+++ src/app/github/search/search.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpService } from '../http/http.service';
-import { User, IState } from '../../store'
+import { User, IState } from '../../store';
 import { SearchResponse } from './response';
 import { NgRedux } from '@angular-redux/store';
 import { Actions } from 'src/app/actions';
@@ -37,7 +37,7 @@ export class SearchComponent implements OnDestroy {
     this.httpService.getUser(handle).subscribe(
       (response: SearchResponse) => {
         console.debug("sucess : ", response);
-        this.ngRedux.dispatch(this.actions.search(response.items))
+        this.ngRedux.dispatch(this.actions.search(response.items));
       },
       error => {
         console.error("error : ", error);
